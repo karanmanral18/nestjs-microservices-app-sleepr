12d64647f95e993f3e2ee87fb4dcf847b4393587
keith.karan <keith.karan@rubicotech.in>
2024-07-08 17:22:20 +0530

    add database and config service

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 nest generate library common
 
-#
+#Create modules in common
+
+nest g mo config -p common 
+nest g mo database -p common
---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        dbName: configService.get('MONGODB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
